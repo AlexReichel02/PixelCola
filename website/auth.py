ab38dd90eae1437f5bb81b46d8ca028d39e18458
Josh6464 <jld106@students.uwf.edu>
2023-09-13 00:35:57 -0500

    centered email and password textbox, added

--- website/auth.py
+++ website/auth.py
@@ -8,8 +8,9 @@ from flask_login import login_user, login_required, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
 @auth.route('/')
-def Login_Music():
+def Login_Music():  #plays music in the login screen
       return render_template('static', 'Elektronomia & RUD - Rollercoaster.mp3')
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
